9b2069ebd9a657650709bd0a78f0852c3f850406
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-09-12 12:26:18 -0700

    trying to add image to file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import NasaGrid from "./components/NasaGrid";
-import { Button } from 'reactstrap';
 import styled from "styled-components";
 import "./App.css";
 
@@ -10,21 +9,36 @@ text-align: center;
 `;
 const Redh1 = styled.h1`
 color:red;
-`
+`;
+const Button = styled.button`
+    padding: 6px 10px;
+    margin: 5px;
+    border: none;
+    border-radius: 3px;
+    color: white;
+
+    ${props => (props.type === 'primary' ? `background: #2196f3;` : null)}
+    ${props => (props.type === 'success' ? `background: #4caf50;` : null)}
+    ${props => (props.type === 'danger' ? `background: #f44336;` : null)}
+    ${props => (props.type === 'warning' ? `background: #fdd835;` : null)}
+`;
 
 function App() {
   return (
     <div className="App">
       <div className="nav">
+        <img src = "components/img/nasa.jpg"/>
         <a href="#">Home</a>
         <a href="#">Planets</a>
         <a href="#">About</a>
         <a href="#">Contact</a>
       </div>
       <NasaGrid />
-      <Button color="danger">Next!</Button>
+      <Button type="danger">Next!</Button>
+      <Button type="warning">warning!</Button>
+      <Button type="primary">Normal!</Button>
       <WrapperDiv>
-        <Redh1>Jordans Styled Component</Redh1>
+        <Redh1>Jordan's Styled Component</Redh1>
       </WrapperDiv>
     </div>
   );
